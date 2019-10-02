df607fb6cf06fb5bb8ee3a14d29afd2b591a78ff
Franklin-Kimatu <frankimatu@gmail.com>
2019-10-02 11:49:40 +0300

    Adding a custom adapter

--- app/src/main/java/com/moringa/myrestaurants/MyRestaurantsArrayAdapter.java
+++ app/src/main/java/com/moringa/myrestaurants/MyRestaurantsArrayAdapter.java
@@ -1,4 +1,17 @@
 package com.moringa.myrestaurants;
 
-public class MyRestaurantsArrayAdapter {
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+public class MyRestaurantsArrayAdapter extends ArrayAdapter {
+    private Context mContent;
+    private String[] mRestaurants;
+    private String[] mCuisines;
+
+    public MyRestaurantsArrayAdapter(Context mContent,int resource,String[] mRestaurants,String[] mCuisines){
+        super(mContent,resource);
+        this.mContent =mContent;
+        this.mCuisines =mCuisines;
+        this.mRestaurants =mRestaurants;
+    }
 }
